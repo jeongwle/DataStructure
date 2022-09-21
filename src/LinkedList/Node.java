047e9a5d8cb78906b9be50ab4360c7a6e7638d78
jeongwle <dldl4568@naver.com>
2022-09-21 19:18:55 +0900

    [Modify] Node 클래스 참조변수 추가 및 생성자 주석  - 주석한 생성자는 쓸모없다고 확신할 때 지울 예정.  - 마지막 노드를 편하게 삭제하기 위해 prev추가. 그런데 이게 더 힘든일이진 않나    하는 생각이 살짝 드는중.

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -1,23 +1,25 @@
 package LinkedList;
 
-public class Node<T> {
-    public T data;
+public class Node {
+    public Object data;
     public Node next;
+    public Node prev;
 
-    public Node() {
-        this.data = null;
-        this.next = null;
-    }
+//    public Node() {
+//        this.data = null;
+//        this.next = null;
+//    }
 
-    public Node(T data) {
+    public Node(Object data) {
         this.data = data;
         this.next = null;
+        this.prev = null;
     }
 
-    public Node(T data, Node next) {
-        this.data = data;
-        this.next = next;
-    }
+//    public Node(Object data, Node next) {
+//        this.data = data;
+//        this.next = next;
+//    }
 
     public String toString() {
         return String.valueOf(this.data);
