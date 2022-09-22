9f0f27459752b5de6948685755516dd4fcfb93a3
jeongwle <dldl4568@naver.com>
2022-09-22 18:03:35 +0900

    [Feat] 테스트 Main   - add, remove, clear, contains 이렇게 몇가지만 테스트해보는 코드   - 에러는 일부러 안냈음.

---
+++ src/LinkedList/Main.java
@@ -0,0 +1,161 @@
+package LinkedList;
+
+import java.text.MessageFormat;
+import java.util.LinkedList;
+
+@SuppressWarnings({"rawtypes", "unchecked", "ConstantConditions"})
+public class Main {
+    public static void main(String[] args) {
+        LinkedList origin = new LinkedList();
+        MyLinkedList mine = new MyLinkedList();
+
+        printBoth(origin, mine);
+        addBoth(origin, mine);
+        removeBoth(origin, mine);
+        clearAndIsEmptyBoth(origin, mine);
+        containsBoth(origin, mine);
+    }
+
+    static void printBoth(LinkedList origin, MyLinkedList mine) {
+        String msgOrigin = "\nOrigin\n Element : {0} \n Size : {1} \n";
+        String msgMine = "Mine\n Element : {0} \n Size : {1} \n";
+
+        String result = MessageFormat.format(msgOrigin, origin, origin.size());
+        System.out.println(result);
+        result = MessageFormat.format(msgMine, mine, mine.size());
+        System.out.println(result);
+        System.out.println("-----------------------------");
+        System.out.println();
+    }
+
+    static void containsBoth(LinkedList origin, MyLinkedList mine) {
+        Object o = 3;
+        System.out.println("boolean contains(Object o)");
+        System.out.println("Object = " + o);
+        boolean originRes = origin.contains(o);
+        boolean mineRes = mine.contains(o);
+        System.out.println("originRes = " + originRes);
+        System.out.println("mineRes = " + mineRes);
+        printBoth(origin, mine);
+    }
+
+    static void clearAndIsEmptyBoth(LinkedList origin, MyLinkedList mine) {
+        System.out.println("void clear()");
+        origin.clear();
+        mine.clear();
+        printBoth(origin, mine);
+
+        System.out.println("boolean isEmpty()");
+        System.out.println("origin.isEmpty() = " + origin.isEmpty());
+        System.out.println("mine.isEmpty() = " + mine.isEmpty());
+        System.out.println("\n-------------------------\n");
+
+        for (int i = 1; i < 5; i++) {
+            origin.add(i);
+            mine.add(i);
+        }
+        origin.add(3);
+        mine.add(3);
+    }
+
+    static void removeBoth(LinkedList origin, MyLinkedList mine) {
+        switch (1) {
+            case 1: {
+                System.out.println("Object remove()");
+                Object originRes = origin.remove();
+                Object mineRes = mine.remove();
+                System.out.println("originRes = " + originRes);
+                System.out.println("mineRes = " + mineRes);
+                printBoth(origin, mine);
+            }
+            case 2: {
+                Object o = 4;
+                System.out.println("boolean remove(Object o)");
+                boolean originRes = origin.remove(o);
+                boolean mineRes = mine.remove(o);
+                System.out.println("Object = " + o);
+                System.out.println("originRes = " + originRes);
+                System.out.println("mineRes = " + mineRes);
+                printBoth(origin, mine);
+
+                o = "bye";
+                originRes = origin.remove(o);
+                mineRes = mine.remove(o);
+                System.out.println("Object = " + o);
+                System.out.println("originRes = " + originRes);
+                System.out.println("mineRes = " + mineRes);
+                printBoth(origin, mine);
+            }
+            case 3: {
+                System.out.println("Object remove(int index)");
+                Object originRes = origin.remove(1);
+                Object mineRes = mine.remove(1);
+                System.out.println("originRes = " + originRes);
+                System.out.println("mineRes = " + mineRes);
+                printBoth(origin, mine);
+            }
+            case 4: {
+                System.out.println("Object removeFirst()");
+                Object originRes = origin.removeFirst();
+                Object mineRes = mine.removeFirst();
+                System.out.println("originRes = " + originRes);
+                System.out.println("mineRes = " + mineRes);
+                printBoth(origin, mine);
+            }
+            case 5: {
+                System.out.println("Object removeLast()");
+                Object originRes = origin.removeLast();
+                Object mineRes = mine.removeLast();
+                System.out.println("originRes = " + originRes);
+                System.out.println("mineRes = " + mineRes);
+                printBoth(origin, mine);
+            }
+        }
+    }
+
+    static void addBoth(LinkedList origin, MyLinkedList mine) {
+        switch (1) {
+            case 1: {
+                Object o = 1;
+                System.out.println("boolean add(Object o)");
+                boolean originResult = origin.add(o);
+                boolean mineResult = mine.add(o);
+                System.out.println("originResult = " + originResult);
+                System.out.println("mineResult = " + mineResult);
+                printBoth(origin, mine);
+            }
+            case 2: {
+                Object o = "hello";
+                System.out.println("void add(int index, Object o)");
+                origin.add(1, o);
+                mine.add(1, o);
+                printBoth(origin, mine);
+            }
+            case 3: {
+                Object o = true;
+                System.out.println("void addFirst(Object o)");
+                origin.addFirst(o);
+                mine.addFirst(o);
+                printBoth(origin, mine);
+            }
+            case 4: {
+                Object o = "bye";
+                System.out.println("void addLast(Object o)");
+                origin.addLast(o);
+                mine.addLast(o);
+                printBoth(origin, mine);
+            }
+            default: {
+                origin.add(1);
+                origin.add(2);
+                origin.add(3);
+                origin.add(2);
+                mine.add(1);
+                mine.add(2);
+                mine.add(3);
+                mine.add(2);
+                printBoth(origin, mine);
+            }
+        }
+    }
+}
